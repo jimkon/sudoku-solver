9c81d2e136d82d83ee68837c41a2717baee66c75
jimkon <kontzedakis_93@hotmail.com>
2018-11-13 01:35:01 +0200

    perspective transfomation

--- src/sudoku_solver.py
+++ src/sudoku_solver.py
@@ -49,16 +49,19 @@ def run():
 
         display_image = gray_to_rgb(gray)
 
-        points = detect(filtered_image)
+        points = np.array(detect(filtered_image))
         for i, p in enumerate(points):
             c_p = [(255, 0, 0),
                    (255, 0, 255),
                    (0, 0, 255),
                    (0, 0, 255)]
             c = c_p[i]
-            cv2.circle(display_image, tuple(p),  3, c, -1)
-            prev_p = points[i-1 if i > 0 else len(c_p)-1]
-            cv2.line(display_image, tuple(p), tuple(prev_p), (255, 0, 0), 1)
+            prev_p = points[i-1]
+            y1, x1 = p
+            y2, x2 = prev_p
+
+            cv2.circle(display_image, (x1, y1),  3, c, -1)
+            cv2.line(display_image, (x1, y1), (x2, y2), (255, 0, 0), 1)
 
         # Display the fps
         cv2.putText(display_image, "fps:{}".format(fps), (20, 20),
@@ -73,10 +76,8 @@ def run():
             cap.release()
             cv2.destroyAllWindows()
 
-            sudoku_image = crop_image(gray, points)
-
-            sudoku_image = cv2.resize(crop_image(gray, points), (9*DIGIT_RESOLUTION[0], 9*DIGIT_RESOLUTION[1]),
-                                      interpolation=cv2.INTER_CUBIC)  # INTER_AREA  for shrinking
+            sudoku_image = crop_and_resize_image(gray, points,
+                                                 new_shape=(9*DIGIT_RESOLUTION[0], 9*DIGIT_RESOLUTION[1]))
 
             digits = crop_digits(sudoku_image)
 
@@ -150,15 +151,42 @@ def detect(filtered_img):
     w = length
     h = length
 
-    return [x, y], [x+w, y], [x+w, y+h], [x, y+h]
+    return [y+200, x-20], [y, x+w+100], [y+h, x+w], [y+h-200, x]
+    # return [y, x], [y, x+w], [y+h, x+w], [y+h, x]
+    # return [y, x+10], [y, x+w+100], [y+h, x+w], [y+h, x]
 
 
-def crop_image(img, points):
-    x1 = points[0][0]
-    x2 = points[1][0]
-    y1 = points[0][1]
-    y2 = points[2][1]
-    return img[y1:y2, x1:x2]
+def crop_and_resize_image(img, points, new_shape=None):
+    if new_shape is None:
+        new_shape = img.shape
+
+    def order_points(pts):
+        # https://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/
+        rect = np.zeros((4, 2), dtype="float32")
+        s = pts.sum(axis=1)
+        rect[0] = pts[np.argmin(s)]
+        rect[2] = pts[np.argmax(s)]
+        diff = np.diff(pts, axis=1)
+        rect[1] = pts[np.argmin(diff)]
+        rect[3] = pts[np.argmax(diff)]
+        return rect
+
+    tl, tr, br, bl = order_points(points)
+    w = new_shape[1]
+    h = new_shape[0]
+    a1, a2 = tl[::-1], [0, 0]
+    b1, b2 = tr[::-1], [0, w]
+    c1, c2 = br[::-1], [h, w]
+    d1, d2 = bl[::-1], [h, 0]
+    pts1 = np.float32([a1, b1, c1, d1])
+    pts2 = np.float32([a2, b2, c2, d2])
+
+    M = cv2.getPerspectiveTransform(pts1, pts2)
+    print(M)
+
+    res = cv2.warpPerspective(img, M, (w, h))
+
+    return res
 
 
 def crop_digits(img):
@@ -166,8 +194,8 @@ def crop_digits(img):
 
     points = np.multiply(np.array(list(product(np.arange(9), np.arange(9)))), DIGIT_RESOLUTION)
 
-    res = np.array(list(img[p[1]:p[1]+DIGIT_RESOLUTION[1], p[0]
-                   :p[0]+DIGIT_RESOLUTION[0]] for p in points))
+    res = np.array(list(img[p[1]: p[1]+DIGIT_RESOLUTION[1], p[0]
+                   : p[0]+DIGIT_RESOLUTION[0]] for p in points))
     res = np.reshape(res, (9, 9, DIGIT_RESOLUTION[0], DIGIT_RESOLUTION[1]))
 
     return res
@@ -197,7 +225,7 @@ def extend_image(img, new_shape, extention_value=0):
     x, y = int((new_shape[1]-img.shape[1])/2), int((new_shape[0]-img.shape[0])/2)
     w, h = img.shape[1], img.shape[0]
 
-    new_img[y:y+h, x:x+w] = img
+    new_img[y: y+h, x: x+w] = img
 
     return new_img
 
@@ -208,7 +236,7 @@ def table_to_string(unsolved_sudoku):
 
 
 def string_to_table(s):
-    res = np.array(list(np.array(list(int(d) for d in s[i:i+9])) for i in np.arange(9)*9))
+    res = np.array(list(np.array(list(int(d) for d in s[i: i+9])) for i in np.arange(9)*9))
     return res
 
 
@@ -232,26 +260,6 @@ def table_to_image(table, color_mask=[0, 0, 0.8]):
             res = np.hstack([res, t_res])
 
     return gray_to_rgb(res, mask=color_mask)
-
-
-def line_intersect(A1, A2, B1, B2):
-    # https://stackoverflow.com/questions/20677795/how-do-i-compute-the-intersection-point-of-two-lines-in-python
-    def slope(P1, P2):
-        return(P2[1] - P1[1]) / (P2[0] - P1[0])
-
-    def y_intercept(P1, slope):
-        return P1[1] - slope * P1[0]
-
-    m1, m2 = slope(A1, A2), slope(B1, B2)
-    if m1 == m2:
-        print("These lines are parallel!!!")
-        return None
-
-    b1, b2, = y_intercept(A1, m1), y_intercept(B1, m2)
-
-    x = (b2 - b1) / (m1 - m2)
-    y = m1 * x + b1
-    return x, y
 
 
 def rho_theta_to_coords(line, image_shape=None):
@@ -340,6 +348,30 @@ def rho_theta_to_coords(line, image_shape=None):
     x2 = int(x0 - 2000*(-b))
     y2 = int(y0 - 2000*(a))
     return x1, y1, x2, y2
+
+
+def line_intersect(A1, A2, B1, B2):
+    # https://stackoverflow.com/questions/20677795/how-do-i-compute-the-intersection-point-of-two-lines-in-python
+    def slope(P1, P2):
+        if P2[1] - P1[1] == 0:
+            if P2[0]-P1[0] >= 0:
+                return np.finfo(np.float).max
+            else:
+                return np.finfo(np.float).min
+        return(P2[0] - P1[0]) / (P2[1] - P1[1])
+
+    def y_intercept(P1, slope):
+        return P1[0] - slope * P1[1]
+
+    m1, m2 = slope(A1, A2), slope(B1, B2)
+    if m1 == m2:
+        return None
+
+    b1, b2, = y_intercept(A1, m1), y_intercept(B1, m2)
+
+    x = (b2 - b1) / (m1 - m2)
+    y = m1 * x + b1
+    return [y, x]
 
 
 if __name__ == "__main__":
